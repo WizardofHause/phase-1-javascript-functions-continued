98116afadb9a286a1b7d2b064dbe01cacf7b3fcb
Jon Hause <jonrhause@gmail.com>
2022-09-20 17:16:08 -0600

    commit tree f2182147c86c13beff5d7559dad4e7636a5a70e9

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// code your solution here
+function saturdayFun (action = 'roller-skate') {
+    return `This Saturday, I want to ${action}!`;
+}
+
+function mondayWork (workThing = 'go to the office') {
+    return  `This Monday, I will ${workThing}.`;
+}
+
+/*function wrapAdjective () {
+    return function inner(specialParam = 'special', flairParam = '*') {
+        return `You are ${specialParam}!`;
+        };
+    }*/
+
+let wrapAdjective = function (flair = '*') {
+    return function (single='special') {
+        return `You are ${flair}${single}${flair}!`;
+    }
+}
